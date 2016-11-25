e82ea3a69ecd301215051c33caedda2cac25d897
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2016-11-25 14:55:25 -0800

    Pretty sure it works now

--- js/aryan.js
+++ js/aryan.js
@@ -1,27 +1,98 @@
-var sound
+// GLOBAL VARIABLES
+var sound;
+var currId = 0;
+var sprites = new Object();
+
+function hmsToSecondsOnly(str) {
+	// From StackOverflow: http://stackoverflow.com/questions/9640266/convert-hhmmss-string-to-seconds-only-in-javascript
+    var p = str.split(':'),
+        s = 0, m = 1;
+
+    while (p.length > 0) {
+        s += m * parseInt(p.pop(), 10);
+        m *= 60;
+    }
+
+    return s*1000;
+}
+
+function preprocessTags() {
+	playerElements = $(".player")
+	console.log(playerElements)
+	for (var i = playerElements.length - 1; i >= 0; i--) {
+		var el = playerElements[i]
+		var innerText = el.textContent.replace("[", "").replace("]", "")
+		var startEnds = innerText.split("-")
+
+		var linkEl = document.createElement("a")
+		linkEl.innerHTML = "[" + innerText + "]"
+
+		var startAttr = document.createAttribute("start")
+		var startTime = hmsToSecondsOnly(startEnds[0])
+		startAttr.value = startTime
+		
+		// Start, End, Durations
+		var startAttr = document.createAttribute("start")
+		var startTime = hmsToSecondsOnly(startEnds[0])
+		startAttr.value = startTime
+		var endAttr = document.createAttribute("end")
+		var endTime = hmsToSecondsOnly(startEnds[1])
+		endAttr.value = endTime
+		var durationAttr = document.createAttribute("duration")
+		var duration = endTime - startTime
+		durationAttr.value = duration
+
+		// Get sprite name
+		var spriteName = "sprite" + currId
+		currId++;
+		sprites[spriteName] = [startTime, duration]
+		var spriteNameAttr = document.createAttribute("spriteName")
+		spriteNameAttr.value = spriteName
+
+		// HTML Class Name
+		var linkClassAttr = document.createAttribute("class")
+		linkClassAttr.value = "innerPlayer"
+
+		// Set Attributes
+		linkEl.setAttributeNode(startAttr)
+		linkEl.setAttributeNode(endAttr)
+		linkEl.setAttributeNode(durationAttr)
+		linkEl.setAttributeNode(linkClassAttr)
+		linkEl.setAttributeNode(spriteNameAttr)
+
+		el.innerHTML = ""
+		el.appendChild(linkEl)
+	};
+}
+
 function initMusic() {
-	var sound = new Howl({
-		urls: ['soundfile.mp3'],
-		autoplay: false,
-		loop: false,
-		volume: 1,
-		onend: function() {
-			alert('Finished!');
-		}
+	sound = new Howl({
+		src: ['soundfile.mp3'],
+		html5 : true,
+		sprite : sprites
 	});
+}
+
+function bindClickEvents() {
+	$(".innerPlayer").click( function(evt) {
+		el = evt.toElement
+		console.log(el)
+		var spriteName = el.getAttribute("spriteName")
+		console.log(spriteName)
+		// var currSprite = sprites[spriteName]
+		sound.play(spriteName)
+	});
+
 }
 
 function playSegment(startTimeTxt, endTimeTxt) {
 
 }
 
+
 $(document).ready(function() {
-	// Bind play click events
-	$(".innerPlayer").click( function(evt) {
-		el = evt.toElement
-		console.log(el)
-	});
-
-
-
+	preprocessTags();
+	initMusic();
+	bindClickEvents();
+	
 });
--- js/preloader.js
+++ js/preloader.js
@@ -1,56 +1,9 @@
-var currId = 0;
-
-function hmsToSecondsOnly(str) {
-	// From StackOverflow: http://stackoverflow.com/questions/9640266/convert-hhmmss-string-to-seconds-only-in-javascript
-    var p = str.split(':'),
-        s = 0, m = 1;
-
-    while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10);
-        m *= 60;
-    }
-
-    return s;
-}
 
 
 $(document).ready(function() {
 	// Bind play click events
-	playerElements = $(".player")
-	console.log(playerElements)
-	for (var i = playerElements.length - 1; i >= 0; i--) {
-		var el = playerElements[i]
-		var innerText = el.textContent.replace("[", "").replace("]", "")
-		var startEnds = innerText.split("-")
-
-		var linkEl = document.createElement("a")
-		linkEl.innerHTML = "[" + innerText + "]"
-		
-		var startAttr = document.createAttribute("start")
-		var startTime = hmsToSecondsOnly(startEnds[0])
-		startAttr.value = startTime
-
-		var endAttr = document.createAttribute("end")
-		var endTime = hmsToSecondsOnly(startEnds[1])
-		endAttr.value = endTime
-
-		var durationAttr = document.createAttribute("duration")
-		var duration = endTime - startTime
-		durationAttr.value = duration
-
-
-		var linkClassAttr = document.createAttribute("class")
-		linkClassAttr.value = "innerPlayer"
-
-		linkEl.setAttributeNode(startAttr)
-		linkEl.setAttributeNode(endAttr)
-		linkEl.setAttributeNode(durationAttr)
-		linkEl.setAttributeNode(linkClassAttr)
-
-		el.innerHTML = ""
-		el.appendChild(linkEl)
-	};
-
+	
+	
 
 
 });
